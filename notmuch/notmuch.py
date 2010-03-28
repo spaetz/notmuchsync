36dcadb4de6b415f14e0601d17924dfa98bce119
Sebastian Spaeth <Sebastian@SSpaeth.de>
2010-03-28 03:04:49 +0200

    Remove old notmuch backend, use cnotmuch bindings.
    This still needs cleanup and documentation, but it worked on my box.
    
    Signed-off-by: Sebastian Spaeth <Sebastian@SSpaeth.de>

--- notmuch/notmuch.py
+++ notmuch/notmuch.py
@@ -15,10 +15,11 @@
 """
 
 import subprocess, re, logging, os, time, email.utils
-import simplejson as json
+from cnotmuch.notmuch import Database, Query
+from cnotmuch.message import Messages, Message
 
 #---------------------------------------------------------------------------
-class Message(object):
+class SyncMessage(Message):
 #---------------------------------------------------------------------------
     """
     Represents a message as returned by notmuch.
@@ -43,87 +44,24 @@ class Message(object):
       .is_valid: is True if parsing found a message, False otherwise
     """
 
-    def __init__(self, nm_msg):
-       """inititialize a message from "nm_msg" 
+    def __init__(self, msg):
+       """inititialize a message from a Message() object"
 
-          :param nm_msg: a message as output from notmuch show in json format
        """
-       self.is_valid=False
-       self._date=None
-       self.msg=None
-       self.maildirflags, self.sync_maildirflags = None, None
-       self._tags, self.sync_tags = None, None
-       self.parse(nm_msg)
+       #Init this SyncMessage instance with the pointer to the notmuch_message_t
+       super(SyncMessage, self).__init__(msg._msg, msg._parent)
+       self.filename = self.get_filename()
+       flags = re.sub('^.*:[12],([A-Z]*)$','\\1',self.filename)
+       self.maildirflags = set(flags)
+       self.sync_maildirflags = None
+       self.tags = set(self.get_tags())
+       self.sync_tags = None
 
-    @property
-    def file(self):
-        return self.msg.get('filename', None)
-
-    @property
-    def headers(self):
-        return self.msg['headers']
-
-    @property
-    def Subject(self):
-        return self.headers.get('Subject', None)
-
-    @property
-    def From(self):
-        return self.headers.get('From', None)
-
-    @property
-    def realName(self):
-        addr = email.utils.parseaddr(self.From)
-        if (addr[0] == ''):
-            return addr[1]
-        return addr[0]
-
-    @property
-    def mailAddress(self):
-        return email.utils.parseaddr(self.From)[1]
-
-    @property
-    def body(self):
-        return self.msg.get('body', None)
-
-    def get_date(self, format):
-        if self._date is None:
-            self._date = email.utils.parsedate(self.Date)
-        return time.strftime(format, self._date)
-
-    @property
-    def Date(self):
-        return self.headers.get('Date', None)
-
-    @property
-    def id(self):
-        return self.msg.get('id', None)
-
-    def get_tags(self):
-        return self._tags
-    def set_tags(self, tags):
-        self._tags = set(tags)
-    tags = property(get_tags, set_tags)
-
-
-    def parse(self, message):
-        if message == "":
-            logging.debug("Message.parse() was handed an empty text.")
-            return
-        #message[0][0].keys()=[u'body', u'tags', u'filename', u'headers', u'id', u'match']
-        if set((u'filename', u'id')) < set(message.keys()):
-            self.is_valid=True
-            self.msg = message
-            self.tags = message.get('tags',None)
-            flags = re.sub('^.*:[12],([A-Z]*)$','\\1',self.file)
-            self.maildirflags = set(flags)
-        else:
-            #TODO better output here
-            logging.warning("no valid mail")
-        
-    def __repr__(self):
-        """A message is represented by "id:blah (name)" (if valid) or 'NULL' """
-        return "id:%s (%s) %s" % (self.id, self.Subject, self.get_date('%d-%m-%y'))#self.realName)
+    def __del__(self):
+        #override __del__ to essentially do nothing. We don't want to free
+        #self._msg in this inherited instance, as it is freed in the Message()
+        #this is inherited from. The Message() will otherwise crash, it seems.
+        pass
 
     def sync_msg_tags(self, dryrun=False):
         """ Sync up changed maildir tags and/or notmuch tags.
@@ -134,9 +72,11 @@ class Message(object):
         if (self.sync_maildirflags is not None and self.maildirflags ^ self.sync_maildirflags):
             #sync_tags differs from tags. Need to sync up maildir tags
             newtags = "".join(sorted(self.sync_maildirflags))
-            newfile = re.sub(r'^(.*:[12],)([A-Z]*)$',"\\1"+newtags,self.file)
+            newfile = re.sub(r'^(.*:[12],)([A-Z]*)$',"\\1"+newtags,self.filename)
             logging.debug("Maildir flags for %s (%s -> '%s')" % 
-                          (self.id,sorted(self.maildirflags),newtags))
+                          (self.get_message_id(),
+                           sorted(self.maildirflags),
+                           newtags))
             #check if we need to move from 'new' to 'cur' dir
             if 'S' in self.sync_maildirflags:
                 # split BASEDIR / NEW / FILENAME into components
@@ -147,149 +87,31 @@ class Message(object):
 
             if not dryrun:
                 try:
-                    os.rename(self.file, newfile)
+                    os.rename(self.filename, newfile)
                 except OSError, e:
                     if e.errno == 2:
-                        logging.info("Renaming not possible, file %s not found" % (self.file))
+                        logging.info("Renaming not possible, file %s not found"
+                                     % (self.filename))
                     else:
                         raise OSError(e)
 
         if (self.sync_tags is not None
             and self.tags ^ self.sync_tags):
             #sync_notmuchtags differs. Need to sync notmuch tags
-            #list of tags to delete and add e.g. ("-inbox -unread")
-            modtags  = " ".join(map(lambda x:"-"+x,self.tags - self.sync_tags))
-            modtags += " ".join(self.sync_tags - self.tags)
-
-            logging.debug("Sync tag %s for id:%s" % (modtags, self.id))
+            logging.debug("Sync tags: +%s -%s for id:%s" %
+                          (list(self.sync_tags-self.tags),
+                           list(self.tags-self.sync_tags),
+                           self.get_message_id()))
 
             if not dryrun:
                 #actually modify the notmuch tag database
-                process = Notmuch().exec_cmd(["tag",modtags, 
-                                  "--", "id:%s" % (self.id)])
-                (stdout, stderr) = process.communicate()
-                if stderr:
-                    logging.error("Notmuch failed: %s" % (stderr))
-
-
-    def unlink_file(self):
-        """ returns 1 on success, or 0 otherwise.
-        Does not fail if the file does not exist."""
-        logging.debug("Delete %s" % (self.file))
-        try:
-            os.unlink(self.file)
-        except OSError, e:
-            if e.errno == 2:
-                logging.info("File %s not found for deletion." % (self.file))
-                return 0
-            else:
-                raise OSError(e)
-        return 1
-
-#---------------------------------------------------------------------------
-class Thread:
-#---------------------------------------------------------------------------
-    """
-    An iterator containing a bunch of mail messages retaining the thread structure).
-
-    The work horse is self.msgs which is a list of (depth, Message()) tuples.
-    depth starts at 0 and increases for replies.
-    """
-
-    def __init__(self, json_thread=None, keep_body=True, keep_nonmatch=True, depth=0):
-        """ Initialize Thread with a json thread object 
-        """
-        self.msgs = []
-        """(depth, Message())"""
-        self.depth=depth
-        self.keep_body=keep_body
-        self.keep_nonmatch=keep_nonmatch
-        self.is_valid = False
-        if json_thread is not None:
-            self.parse_thread(json_thread, keep_body)
-
-    def parse_forest(self, json_threads):
-        """ parses and add multiple 'threads' (json object)
-        
-        The method honors existing self.keep_body and self.keep_nonmatch values
-        returns: self
-        """
-        if len(json_threads) == 0:
-            logging.debug("Received no messages")
-        
-        for thread in json_threads:
-            t = Thread(thread,keep_nonmatch=self.keep_nonmatch,
-                       keep_body=self.keep_body, depth=self.depth)
-            if t.is_valid:
-                self.msgs.extend(t.msgs)
-        return self
-
-    def parse_thread(self, json_thread, keep_body):
-        """ parses a json_thread object and adds (depth, Message()) tuples to
-        self.msg
-        """
-        for tree in json_thread:
-            msg, reply_thread = tree
-            #parse each single message and append it
-            if not keep_body: del(msg[0][0]['body'])
-            msg = Message(msg)
-            if msg.is_valid:
-                nextdepth=self.depth #by default don't increase the depth
-                if msg.msg['match'] or self.keep_nonmatch:
-                    self.is_valid = True
-                    self.msgs.append((self.depth,msg))
-                    #increase thread depth if we actually add a msg
-                    nextdepth=self.depth+1
-                reply = Thread(reply_thread, keep_body, 
-                               keep_nonmatch=self.keep_nonmatch,depth=nextdepth)
-                if reply.is_valid:
-                    self.is_valid = True
-                    self.msgs.extend(reply.msgs)
-
-    def __len__(self):
-        """ Returns the number of contained messages """
-        return len(self.msgs)
-
-    def __iter__(self):
-        #Just return self
-        self._iter_pos = 0
-        return self
-
-    def next(self):
-        """ returns the next message when iterating """
-        if (self._iter_pos == len(self.msgs)):
-            raise StopIteration     # end of iteration
-        self._iter_pos += 1
-        return self.msgs[self._iter_pos - 1]
-
-    def __repr__(self):
-        lines = []
-        for (depth, msg) in self.msgs:
-            lines.append("%s%s" %(" "*depth,str(msg)))
-        return "\n".join(lines)
-
-    def __len__(self):
-        """ Returns the number of contained messages """
-        return len(self.msgs)
-
-    def __iter__(self):
-        #Just return self
-        self._iter_pos = 0
-        return self
-
-    def next(self):
-        """ returns the next message when iterating """
-        if (self._iter_pos == len(self.msgs)):
-            raise StopIteration     # end of iteration
-        self._iter_pos += 1
-        return self.msgs[self._iter_pos - 1]
-
-
-#---------------------------------------------------------------------------
-class NotmuchError(Exception):
-#---------------------------------------------------------------------------
-    """This Exception is thrown in case of an error."""
-    pass
+                self.freeze()
+                self.remove_all_tags()
+                for tag in self.sync_tags:
+                    self.add_tag(tag)
+                self.thaw()
+                #TODO: catch error:
+                #logging.error("Notmuch failed: %s" % (stderr))
 
 #---------------------------------------------------------------------------
 class Notmuch:
@@ -305,97 +127,49 @@ class Notmuch:
     :type logger: logging.Logger
     :rtype: the initialized Notmuch instance
     """
-    __notmuchcmd__ = "notmuch"
 
     def __init__(self, logger=None):
-        """
-        Initialize the notmuch object
-
-        """
+        """Initialize the notmuch object"""
         if logger:
             self.logger = logger
         else:
             self.logger = logging.getLogger()
 
-    def exec_cmd(self, cmdoptions):
-        """ Execute a notmuch command and return the 'process'
+        #open the database as read-only
+        self.db_ro = Database()
 
-        :param cmdoptions: is a list of command options to notmuch. You will 
-        usually finish the prosess with something like:
-        (stdout, stderr) = process.communicate()
-        :type cmdoptions: [strings]
-
-        :rtype: process as returned by subprocess.Popen
-        :exception: *:exc:`OSError`: [Errno 2] No such file or directory* if notmuch is not installed or in the searchable path
-        """
-        logging.debug("Execute %s" % ([Notmuch.__notmuchcmd__] + cmdoptions))
-
-        process = subprocess.Popen([Notmuch.__notmuchcmd__] + cmdoptions, 
-                                   stdout=subprocess.PIPE,
-                                   stderr=subprocess.PIPE)
-        return process
-
-
-    def new(self):
-        """ Perform a notmuch new in order to get a consistent db
-        Returns: True on success or None on error
-        """
-        cmdoptions = ['new']
-        process = self.exec_cmd(cmdoptions)
-        (stdout, stderr) = process.communicate()
-
-        if process.returncode != 0:
-            logging.warning("Notmuch new failed (returns: %d): %s" %
-                          (process.returncode, stderr))
-            return None
-        return True
-
-    def show(self, term, wholeThread=False):
-        """searches notmuch for "term"
-        returns a Thread() object containing all messages, 
-
-        Returns even on an xapian error,
-        as not all of them are fatal and some occur frequently...
-        """
-        cmdoptions = ['show']
-        if wholeThread: cmdoptions.append('--entire-thread')
-        cmdoptions.append('--format=json')
-        cmdoptions.append(term)
-        process = self.exec_cmd(cmdoptions)
-        (stdout, stderr) = process.communicate()
-
-        if process.returncode != 0:
-            logging.error("Notmuch show failed (xapian returns: %d).\n%s" \
-                  % (process.returncode, stderr))
-            raise NotmuchError(stderr)
-
-        json_forest = json.loads(stdout)
-        return Thread(keep_nonmatch=wholeThread).parse_forest(json_forest)
-
-    def prune(self, criteria="tag:delete or tag:maildir::trashed", dryrun=False):
+    def prune(self, crit="tag:delete or tag:maildir::trashed", dryrun=False):
         """ Physically delete all mail files matching 'tag'. 
         Returns the number of matched mails.
         If dryrun == True, it will not actually delete them.
         """
-        del_msgs = self.show(criteria, wholeThread=False)
-        len_del_msgs = len(del_msgs)
+        msgs = Query(self.db_ro,crit).search_messages()
+        to_delete = 0
 
-        if del_msgs == None:
+        if msgs == None:
             #TODO, catch notmuch.show exceptions
             logging.error("Could not prune messages due to notmuch error.")
             return None
 
-        if not dryrun:
-            deleted = 0
-            for (depth, m) in del_msgs:
-                success = m.unlink_file()
-                deleted += success
-            self.logger.info("Deleted %d of %d messages." %
-                             (deleted, len_del_msgs))
-        else:
+        if dryrun:
+            to_delete = len(msgs)
             self.logger.info("Would have deleted %d messages." %
-                             (len_del_msgs))
-        return len_del_msgs
+                             (to_delete))
+            return to_delete
+
+        deleted = 0
+        for msg in msgs:
+            to_delete += 1
+            try:
+                os.unlink(msg.get_filename())
+                deleted += 1
+            except OSError, e:
+                #skip errors
+                pass
+
+        self.logger.info("Deleted %d of %d messages." %
+                             (deleted, to_delete))
+        return to_delete
 
     def syncTags(self,frommaildir=False,dryrun=False, all_mails=None):
         """ sync the unread Tags. It does not really go through all mail files,
@@ -431,7 +205,14 @@ class Notmuch:
         else:
             searchterm = "0..%d" % (now+2592000)
 
-        msgs = self.show(searchterm, wholeThread=False)
+        #we need a rw database if we might need to modify notmuch tags
+        if frommaildir and not dryrun:
+            db = Database(mode=Database.MODE.READ_WRITE)
+        else:
+            db = self._db
+
+        #fetch all messages
+        msgs = Query(db,searchterm).search_messages()
 
         if msgs == None:
             logging.error("Could not sync messages due to notmuch error.")
@@ -441,7 +222,11 @@ class Notmuch:
         tag_trans_inverse = dict((tag_trans[x], x) for x in tag_trans) # a bit clumsy ?!
         # check all messages for inconsistencies
         num_modified = 0
-        for (depth, m) in msgs:
+        total_msgs   = 0
+        for msg in msgs:
+            #create a derived SyncMessage instance from our Message()
+            m = SyncMessage(msg)
+            total_msgs += 1
             modified = False
             # handle SEEN vs unread tags:
             if not (('S' in m.maildirflags) ^ ('unread' in m.tags)):
@@ -480,4 +265,8 @@ class Notmuch:
                 num_modified += 1 
                 m.sync_msg_tags(dryrun=dryrun)
 
-        logging.info("Synced %d messages. %d modified." % (len(msgs), num_modified))
+            #delete our temporary SyncMessage
+            del(m)
+
+        logging.info("Synced %d messages. %d modified."
+                     % (total_msgs, num_modified))
